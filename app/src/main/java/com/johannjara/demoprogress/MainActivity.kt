45ccb445fd2cad4fc85e78d956768f5e9f26eb4b
Johann Jara <johannfjs@gmail.com>
2022-09-30 09:13:15 -0500

    feat: added progress example

--- app/src/main/java/com/johannjara/demoprogress/MainActivity.kt
+++ app/src/main/java/com/johannjara/demoprogress/MainActivity.kt
@@ -3,41 +3,47 @@ package com.johannjara.demoprogress
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.johannjara.demoprogress.ui.theme.DemoProgressTheme
+import androidx.compose.ui.unit.dp
+import com.johannjara.design.atom.HorizontalDivider
+import com.johannjara.design.atom.Icon
+import com.johannjara.design.molecule.AppBar
+import com.johannjara.design.molecule.CircleProgressBar
+import com.johannjara.design.molecule.ProgressBar
+import com.johannjara.design.theme.DemoProgressTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DemoProgressTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Scaffold(topBar = {
+                    AppBar(
+                        title = "Reto",
+                        actions = {
+                            Icon(onClick = { }, icon = Icons.Filled.Info, description = "infoIcon")
+                        })
+                }) { contentPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(contentPadding)
+                            .padding(all = 16.dp)
+                            .fillMaxHeight()
+                            .fillMaxWidth()
+                    ) {
+                        ProgressBar(text = "Progress Bar")
+                        HorizontalDivider(modifier = Modifier.padding(vertical = 16.dp))
+                        CircleProgressBar(text = "Circle Progress Bar")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DemoProgressTheme {
-        Greeting("Android")
     }
 }
